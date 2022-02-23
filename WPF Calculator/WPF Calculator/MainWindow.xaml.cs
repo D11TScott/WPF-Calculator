5443635894e1f7d9001bbfc49a64a71d50d0db53
D11TScott <t.scott@double11.com>
2022-02-23 16:14:38 +0000

    Operation Buttons

--- WPF Calculator/WPF Calculator/MainWindow.xaml.cs
+++ WPF Calculator/WPF Calculator/MainWindow.xaml.cs
@@ -20,11 +20,60 @@ namespace WPF_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private enum eOperation
+        {
+            e_None,
+            e_Add,
+            e_Sub,
+            e_Div,
+            e_Mul
+        }
+
+        private eOperation m_previousOperation = eOperation.e_None; 
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+        private void PerformCalculation(eOperation operationToPerfom)
+        {
+            List<double> inputtedNumbers = new List<double>();
+            switch (operationToPerfom)
+            {
+                case eOperation.e_None:
+                    break;
+                case eOperation.e_Add:
+                    inputtedNumbers = textDisplay.Text.Split(" + ").Select(double.Parse).ToList();
+                    textDisplay.Text = (inputtedNumbers[0] + inputtedNumbers[1]).ToString();
+                    break;
+                case eOperation.e_Sub:
+                    inputtedNumbers = textDisplay.Text.Split(" - ").Select(double.Parse).ToList();
+                    textDisplay.Text = (inputtedNumbers[0] - inputtedNumbers[1]).ToString();
+                    break;
+                case eOperation.e_Div:
+                    try
+                    {
+                        inputtedNumbers = textDisplay.Text.Split(" ÷ ").Select(double.Parse).ToList();
+                        if (inputtedNumbers[1] == (double)0)
+                        {
+                            throw new DivideByZeroException(); // because apparently it is infinity otherwise... Weird C#
+                        }
+                        textDisplay.Text = (inputtedNumbers[0] / inputtedNumbers[1]).ToString();
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        textDisplay.Text = "ERROR: CAN'T DIVIDE BY ZERO";
+                    }
+                    break;
+                case eOperation.e_Mul:
+                    inputtedNumbers = textDisplay.Text.Split(" x ").Select(double.Parse).ToList();
+                    textDisplay.Text = (inputtedNumbers[0] * inputtedNumbers[1]).ToString();
+                    break;
+                default:
+                    break;
+            }
+            m_previousOperation = eOperation.e_None;
+        }
 
 
         private void onNumberButtonClick(object sender, RoutedEventArgs e)
@@ -38,6 +87,51 @@ namespace WPF_Calculator
             {
                 textDisplay.Text += b.Content.ToString();
             }
+        }
+
+        private void btnMul_Click(object sender, RoutedEventArgs e)
+        {
+            if(m_previousOperation != eOperation.e_None)
+            {
+                PerformCalculation(m_previousOperation);
+            }
+
+            m_previousOperation = eOperation.e_Mul;
+            textDisplay.Text += " x ";
+        }
+
+        private void btnDiv_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_previousOperation != eOperation.e_None)
+            {
+                PerformCalculation(m_previousOperation);
+            }
+
+            m_previousOperation = eOperation.e_Div;
+            textDisplay.Text += " ÷ ";
+        }
+
+        private void btnSub_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_previousOperation != eOperation.e_None)
+            {
+                PerformCalculation(m_previousOperation);
+            }
+
+            m_previousOperation = eOperation.e_Sub;
+            textDisplay.Text += " - ";
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_previousOperation != eOperation.e_None)
+            {
+                PerformCalculation(m_previousOperation);
+            }
+
+            m_previousOperation = eOperation.e_Add;
+            textDisplay.Text += " + ";
+
         }
     }
 }
